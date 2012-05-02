da77600196cd96ae31631a9de5f0944fc6bc92be
Ben Mendis <dragonwisard@gmail.com>
2012-05-02 11:35:13 -0400

    Start with pwd instead of ls.

--- tutlib.sh
+++ tutlib.sh
@@ -11,21 +11,25 @@ then
 fi
 
 read lastcmd
-current_step=$(cat $START/.progress)
+current_step=$(tail -n 1 $START/.progress)
 
+# Step 1: Navigation the directory structre.
 step_1(){
 	case "$current_step" in
 		"1")
 			echo "Welcome to the tutorial!"
-			echo "Let's begin by printing a directory listing."
-			echo "Type 'ls' to continue."
-			echo 1.1>$START/.progress
+			echo "Let's begin by figuring out where we are."
+			echo "To print the present working directory we use the 'pwd' command."
+			echo "Hint: Type 'pwd' to continue."
+			echo 1.1>>$START/.progress
 			;;
 		"1.1")
 			case "$lastcmd" in
-				"ls")
+				"pwd")
+					echo
 					echo "Good job!"
-					echo 1.2>$START/.progress
+					echo "As you can see above, the current directory is \"`pwd`\"."
+					echo 1.2>>$START/.progress
 					;;
 				*)
 					echo "Try again. You typed \"$lastcmd\""
