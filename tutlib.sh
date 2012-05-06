51208dd262f11e959f4ec849563b58b2777673af
Ben Mendis <dragonwisard@gmail.com>
2012-05-06 17:58:01 -0400

    A couple more steps

--- tutlib.sh
+++ tutlib.sh
@@ -20,7 +20,6 @@ step_1(){
 			echo "Welcome to the tutorial!"
 			echo "Let's begin by figuring out where we are."
 			echo "To print the present working directory we use the 'pwd' command."
-			echo "Hint: Type 'pwd' and hit Enter to continue."
 			echo 1.1>>$START/.progress
 			;;
 		"1.1")
@@ -42,6 +41,7 @@ step_1(){
 					;;
 				*)
 					echo "Try again. You typed \"$lastcmd\""
+					echo "Hint: Type 'pwd' and hit Enter to continue."
 					;;
 			esac
 			;;
@@ -52,10 +52,32 @@ step_1(){
 				echo "Let's make a directory to use for the rest of this tutorial. To make a directory"
 				echo "issue the make directory command 'mkdir' with the name of a directory you want to"
 				echo "create. In this case, we'll make the 'tutorial' directory."
-				echo "Hint: Try typing 'mkdir tutorial'"
 				echo 1.3>>$START/.progress
 			else
 				echo "Try again. You typed \"$lastcmd\""
+				echo "Hint: Try typing 'cd'"
+			fi
+			;;
+		"1.3")
+			if [ -d "$HOME/tutorial" ]
+			then
+				echo "You've just created a directory. Now lets change into that directory. To change"
+				echo "directory you use the command 'cd' with the name of the directory you want to"
+				echo "change to."
+				echo 1.4>>$START/.progress
+			else
+				echo "Try again. You typed \"$lastcmd\""
+				echo "Hint: Try typing 'mkdir tutorial'"
+			fi
+			;;
+		"1.4")
+			if [ "`pwd`" = "$HOME/tutorial" ]
+			then
+				echo "Nicely done. You're now in the tutorial directory."
+				echo 1.5>>$START/.progress
+			else
+				echo "Try again. You typed \"$lastcmd\""
+				echo "Hint: Try typing 'cd tutorial'"
 			fi
 			;;
 	esac
